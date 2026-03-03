ab0a4584cac925db6f0ad73287eb773461833f4a
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-03-03 22:37:59 +0300

    Add subtraction and subswequent test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,12 @@
-from src.main import addition
+from src.main import addition, subtract
 
 
 def test_add():
     assert addition(2, 3) == 5
     assert addition(0, 0) == 0
     assert addition(5, 5) == 10
+
+def test_subtract():
+    assert subtract(2, 3) == -1
+    assert subtract(0, 0) == 0
+    assert subtract(10, 5) == 5 
