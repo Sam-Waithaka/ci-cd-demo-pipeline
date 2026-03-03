a861f6795fffad45711eccdd86d5ebb1a080b745
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-03-03 21:54:05 +0300

    Rename function 'add' to 'addition' and update corresponding tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
-from src.main import add
+from src.main import addition
 
 
 def test_add():
-    assert add(2, 3) == 5
-    assert add(0, 0) == 0
-    assert add(5, 5) == 10
+    assert addition(2, 3) == 5
+    assert addition(0, 0) == 0
+    assert addition(5, 5) == 10
